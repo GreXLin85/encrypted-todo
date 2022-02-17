4bf9e6cfe0a7e87fd9ab00dc0fecc5ec6d983857
Fıratcan Ulukaya <mua.firat@gmail.com>
2022-02-17 15:37:52 +0200

    create modal

---
+++ client/src/components/AddTodoModal.jsx
@@ -0,0 +1,12 @@
+import timesIcon from "../assets/img/times.svg";
+import {ColFull, TodoAddModal} from "../assets/style/styled";
+
+const AddTodoModal = () => {
+    return (
+        <TodoAddModal id="studentAddModal">
+            sa
+        </TodoAddModal>
+    );
+}
+
+export default AddTodoModal;
